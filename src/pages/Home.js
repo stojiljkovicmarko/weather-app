30ad191a8e166e9d469c69f3f0377386b6351a7c
Marko Stojiljkovic <stojiljkovicmarko@yahoo.com>
2022-01-15 13:49:13 +0100

    deleted some redundant css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -94,8 +94,6 @@ const Home = () => {
         });
         dispatch({ type: "FETCH_SUCCESS" });
       } catch (error) {
-        console.log(error);
-        console.log(error.message);
         dispatch({ type: "FETCH_FAIL" });
       }
     };
