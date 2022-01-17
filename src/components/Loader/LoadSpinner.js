09ee8dbbbbc6573040d6d0ae49f06f17e6f35ba8
Marko Stojiljkovic <stojiljkovicmarko@yahoo.com>
2022-01-17 15:58:25 +0100

    done with air pollution main part, left the additional data css...

--- src/components/Loader/LoadSpinner.js
+++ src/components/Loader/LoadSpinner.js
@@ -1,11 +1,14 @@
 import classes from "./LoadSpinner.module.css";
+import cloud from "../../assets/cloud.png";
 
 const LoadingSpinner = () => {
   return (
-    <div className={classes.spinner}>
-      <div className={classes.bounce1}></div>
-      <div className={classes.bounce2}></div>
-      <div className={classes.bounce3}></div>
+    <div className={classes.loadspinner}>
+      <div className={classes.spinner}>
+        <div className={classes.bounce1}></div>
+        <div className={classes.bounce2}></div>
+        <div className={classes.bounce3}></div>
+      </div>
     </div>
   );
 };
